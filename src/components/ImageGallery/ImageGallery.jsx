42930e0c66ad3e8da7b80808c1c8dc3a0198d9f3
Arsentosh <doncenkoarsenij185@gmail.com>
2023-11-15 19:40:01 +0200

    fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -13,11 +13,10 @@ export default class ImageGallery extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.inputValue !== this.props.inputValue) {
+    const { inputValue, page } = this.props;
+
+    if (prevProps.inputValue !== inputValue || prevProps.page !== page) {
       this.fetchLoad();
-    }
-    if (prevProps.page !== this.props.page && this.props.page > 1) {
-      this.fetchLoadMore();
     }
   }
 
@@ -26,22 +25,9 @@ export default class ImageGallery extends Component {
 
     getImages(inputValue, page)
       .then(response => {
-        this.setState({
-          images: response.hits,
-          totalHits: response.totalHits,
-          status: 'resolve',
-        });
-      })
-      .catch(error => this.setState({ status: 'rejected' }));
-  };
-
-  fetchLoadMore = () => {
-    const { inputValue, page } = this.props;
-
-    getImages(inputValue, page + 1)
-      .then(response => {
         this.setState(prevState => ({
           images: [...prevState.images, ...response.hits],
+          totalHits: response.totalHits,
           status: 'resolve',
         }));
       })
@@ -55,29 +41,23 @@ export default class ImageGallery extends Component {
       return <Loader />;
     }
 
-    if (status === 'resolve') {
-      return (
-        <>
-          <ul className={s.gallery}>
-            {images.map(({ id, largeImageURL, tags }) => (
-              <ImageGalleryItem
-                key={id}
-                url={largeImageURL}
-                tags={tags}
-                onClick={this.props.onClick}
-              />
-            ))}
-          </ul>
-          {images.length < totalHits && (
-            <Button onClick={this.props.loadMoreBtn} />
-          )}
-          {images.length === 0 && <p>No results found</p>}
-        </>
-      );
-    }
-
-    if (status === 'rejected') {
-      return <p>Error fetching images</p>;
-    }
+    return (
+      <>
+        <ul className={s.gallery}>
+          {images.map(({ id, largeImageURL, tags }) => (
+            <ImageGalleryItem
+              key={id}
+              url={largeImageURL}
+              tags={tags}
+              onClick={this.props.onClick}
+            />
+          ))}
+        </ul>
+        {images.length < totalHits && (
+          <Button onClick={this.props.loadMoreBtn} />
+        )}
+        {images.length === 0 && <p>No results found</p>}
+      </>
+    );
   }
 }
